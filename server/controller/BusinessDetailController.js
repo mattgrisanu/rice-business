972a7cbe7cca5a19f684c37b9911d959a69c3794
Katherine <katherine.sj@gmail.com>
2016-06-23 10:44:01 -0700

    moved business services out to separate repo

---
+++ server/controller/BusinessDetailController.js
@@ -0,0 +1,51 @@
+var BusinessDetail = require('./../models/BusinessDetailModel.js');
+// var PreferenceController = require('./../controller/PreferenceController.js');
+
+
+
+module.exports = {
+  getDetails: function (req, res) {
+    //assuming req.param looks like { business_id: this.props.business_id }
+    var queryObj = {
+      business_id: req.query.business_id
+    }
+    BusinessDetail.where(queryObj).fetch()
+      .then(function (foundDetail) {
+        res.status(200).send(foundDetail);
+      })
+      .catch(function (err) {
+        console.error('Error: Fetching '+ req.params.business_id + ' from db', err);
+        res.status(500).send(err);
+      });
+  },
+
+  _saveDetails: function (business_id, categoriesArr, neighborhoodsArr, res) {
+    var saveToDb = function (arr, count, type) {
+      if (arr === undefined || count === arr.length) {
+        if (res !== undefined) {
+          res.status(201).send('Add success');
+        }
+        return;
+      }
+
+      var newDetail = {
+        business_id: business_id,
+        type: type,
+        value: arr[count]
+      };
+
+      new BusinessDetail(newDetail).save()
+        .then(function (saved) {
+          console.log('Successfull saved BusinessDetail', saved);
+          saveToDb(arr, ++count, type);
+        })
+        .catch(function (err) {
+          console.error('Error: Saving BusinessDetail to the database', err);
+        });
+    };
+
+    saveToDb(categoriesArr, 0, 'category');
+    saveToDb(neighborhoodsArr, 0, 'neighborhood');
+  }
+
+};
---
+++ server/models/BusinessDetailModel.js
@@ -0,0 +1,22 @@
+var db = require('./../config/db.js');
+
+db.knex.schema.hasTable('BusinessDetails').then(function (exists) {
+  if (!exists) {
+    db.knex.schema.createTable('BusinessDetails', function(business) {
+      business.increments('id').primary();
+      business.string('business_id', 255).unique();
+      business.string('type', 255);
+      business.string('value', 255);
+      business.timestamps();
+    }).then(function (table) {
+      console.log('Created "BusinessDetails" Table', table);
+    });
+  }
+});
+
+var BusinessDetail = db.Model.extend({
+  tableName: 'BusinessDetails',
+  hasTimestamps: true
+});
+
+module.exports = BusinessDetail;
