e63519a5ba809ad4a6ef06d0cdb583f7ccbce410
Katherine <katherine.sj@gmail.com>
2016-07-04 10:59:18 -0700

    unignored yelp.js

---
+++ server/config/yelp.js
@@ -0,0 +1,9 @@
+var Yelp = require('yelp');
+var yelp = new Yelp({
+  consumer_key: process.env.YELP_CONSUMER_KEY,
+  consumer_secret: process.env.YELP_CONSUMER_SECRET,
+  token: process.env.YELP_TOKEN,
+  token_secret: process.env.YELP_TOKEN_SECRET,
+})
+
+module.exports = yelp;
